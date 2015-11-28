9a3fef94d28e5c5b0a902d809248294f917d0071
Grigory Perepechko <grigoryp@playtika.com>
2015-11-28 16:35:48 +0300

    Added W10 sample (finalizer thread exceptions are not logged)

---
+++ Src/XLog.Sample.W10/App.xaml.cs
@@ -0,0 +1,74 @@
+﻿using System.IO;
+using System.Threading.Tasks;
+using Windows.ApplicationModel.Activation;
+using Windows.Storage;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using XLog.Formatters;
+using XLog.NET.Targets;
+
+namespace XLog.Sample.W10
+{
+    sealed partial class App
+    {
+        public static Logger Logger;
+
+        public App()
+        {
+            this.InitializeComponent();
+            InitLogger();
+            Logger = LogManager.Default.GetLogger("Program");
+
+
+            Application.Current.UnhandledException += CurrentOnUnhandledException;
+            TaskScheduler.UnobservedTaskException += TaskSchedulerOnUnobservedTaskException;
+        }
+
+        private static void TaskSchedulerOnUnobservedTaskException(object sender, UnobservedTaskExceptionEventArgs e)
+        {
+            Logger.Error("TaskSchedulerOnUnobservedTaskException", e.Exception);
+            LogManager.Default.Flush();
+        }
+
+        private void CurrentOnUnhandledException(object sender, UnhandledExceptionEventArgs e)
+        {
+            Logger.Fatal("CurrentOnUnhandledException", e.Exception);
+            LogManager.Default.Flush();
+        }
+
+        private static void InitLogger()
+        {
+            var formatter = new LineFormatter();
+            var logConfig = new LogConfig(formatter);
+
+            logConfig.AddTarget(LogLevel.Trace, LogLevel.Fatal, new SyncFileTarget(Path.Combine(ApplicationData.Current.LocalFolder.Path, @"Log.log")));
+
+            LogManager.Init(logConfig);
+        }
+
+
+
+
+
+
+
+        protected override void OnLaunched(LaunchActivatedEventArgs e)
+        {
+
+            Frame rootFrame = Window.Current.Content as Frame;
+
+            if (rootFrame == null)
+            {
+                rootFrame = new Frame();
+                Window.Current.Content = rootFrame;
+            }
+
+            if (rootFrame.Content == null)
+            {
+                rootFrame.Navigate(typeof(MainPage), e.Arguments);
+            }
+
+            Window.Current.Activate();
+        }
+    }
+}
---
+++ Src/XLog.Sample.W10/MainPage.xaml.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Threading.Tasks;
+using Windows.UI.Xaml;
+
+namespace XLog.Sample.W10
+{
+    public sealed partial class MainPage
+    {
+        class FinalizeableClass
+        {
+            ~FinalizeableClass()
+            {
+                throw new Exception("Exception in finalizer");
+            }
+        }
+
+        public MainPage()
+        {
+            this.InitializeComponent();
+        }
+
+        private void ThroExceptionOnMainThread(object sender, RoutedEventArgs e)
+        {
+            throw new Exception("Main thread exc");
+        }
+
+        private void ThrowUnobservedTaskException(object sender, RoutedEventArgs e)
+        {
+            Task.Run(() =>
+            {
+                throw new Exception("Unobserved task exc");
+            });
+
+            Task.Delay(50).Wait();
+
+            GC.Collect();
+            GC.WaitForPendingFinalizers();
+            GC.Collect();
+        }
+
+        private void ThrowExceptionOnFinalizerThread(object sender, RoutedEventArgs e)
+        {
+            new FinalizeableClass();
+
+            GC.Collect();
+            GC.WaitForPendingFinalizers();
+            GC.Collect();
+        }
+
+        private async void ThrowAsyncVoidException(object sender, RoutedEventArgs e)
+        {
+            await Task.Delay(100);
+
+            throw new Exception("Async void exception");
+        }
+    }
+}
