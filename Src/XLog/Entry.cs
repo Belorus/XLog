13cd3a1116e0efa95e70d59919fcb0404a0702f2
Grigory Perepechko <grigoryp@playtika.com>
2016-03-11 00:27:40 +0300

    Improved performance even more

--- Src/XLog/Entry.cs
+++ Src/XLog/Entry.cs
@@ -1,9 +1,21 @@
 ﻿using System;
+using System.Diagnostics;
 
 namespace XLog
 {
     public class Entry
     {
+        // This stuff is an optimization. DateTime.Now is a very expensive operation.
+        // It makes 2 calls to OS API. First to get time, seconds to calculate timezone offset
+        private static readonly long BaseTimeTicks;
+        private static readonly Stopwatch Delta;
+
+        static Entry()
+        {
+            BaseTimeTicks = DateTime.Now.Ticks;
+            Delta = Stopwatch.StartNew();
+        }
+
         public LogLevel Level;
         public string LevelStr;
         public string Tag;
@@ -20,7 +32,7 @@ namespace XLog
             Message = message;
             Category = category;
             Exception = ex;
-            TimeStamp = DateTime.Now;
+            TimeStamp = new DateTime(BaseTimeTicks + Delta.ElapsedTicks);
         }
     }
 }
