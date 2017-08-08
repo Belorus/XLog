7ce4cc65a55ec9fc8d55898b7e4a826c80dabca3
Grigory Perepechko <mylce@ya.ru>
2017-08-08 13:39:38 +0300

    Added httpTarget

---
+++ Src/XLog.NET/Targets/HttpTarget.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using XLog.Formatters;
+
+namespace XLog.NET.Targets
+{
+    public class HttpTarget : Target
+    {
+        private readonly string _uri;
+
+        public HttpTarget(string uri, IFormatter formatter = null) : base(formatter)
+        {
+            _uri = uri;
+        }
+
+        public override async void Write(string content)
+        {
+            using (var client = new HttpClient())
+            {
+                var x = await client.PostAsync(_uri, new StringContent(content) { Headers = { ContentType = MediaTypeHeaderValue.Parse("text/plain") }});
+            }
+        }
+    }
+}
