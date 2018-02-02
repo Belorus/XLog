19fda30009d83b4bbcc13dcf34be675e6891f767
Yaroslav Shabanets <YaroslavS@playtika.com>
2018-02-02 16:42:09 +0300

    Separate project for mac

---
+++ Src/XLog.Mac/Properties/AssemblyInfo.cs
@@ -0,0 +1,10 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: ComVisible(false)]
+
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyTitle("XLog.Mac")]
+[assembly: AssemblyProduct("XLog.Mac")]
+[assembly: AssemblyDescription("XLog Mac platform implementation.")]
+[assembly: AssemblyCopyright("Copyright © 2018")]
